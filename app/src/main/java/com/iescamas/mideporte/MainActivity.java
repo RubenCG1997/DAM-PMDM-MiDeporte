e32f9a3d547a08bd9d42a045c70bec08e866e7b9
RubenCG1997 <camachogomez1597@gmail.com>
2024-10-05 17:42:05 +0200

    Ejercicio terminado

--- app/src/main/java/com/iescamas/mideporte/MainActivity.java
+++ app/src/main/java/com/iescamas/mideporte/MainActivity.java
@@ -9,50 +9,82 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     Spinner deportes;
     RadioGroup elecciones;
-    RadioButton eleccion1, eleccion2, eleccion3;
+    RadioButton rb_eleccion1, rb_eleccion2, rb_eleccion3;
     TextView info;
+    ArrayAdapter<CharSequence> adaptador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        eleccion1 = findViewById(R.id.rb_eleccion1);
-        eleccion2 = findViewById(R.id.rb_eleccion2);
-        eleccion3 = findViewById(R.id.rb_eleccion3);
+        rb_eleccion1 = findViewById(R.id.rb_eleccion1);
+        rb_eleccion2 = findViewById(R.id.rb_eleccion2);
+        rb_eleccion3 = findViewById(R.id.rb_eleccion3);
         info = findViewById(R.id.lbl_info);
         deportes = findViewById(R.id.spDeportes);
         elecciones = findViewById(R.id.rg_elecciones);
 
-        ArrayAdapter<CharSequence> adaptador = ArrayAdapter.createFromResource(this,R.array.deportesIndividuales, android.R.layout.simple_spinner_item );
-
-        elecciones.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        //Inicializa el adaptador en deportes individuales por defecto
+        adaptador = ArrayAdapter.createFromResource(this,
+                R.array.deportesIndividuales, android.R.layout.simple_spinner_item);
+        adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        deportes.setAdapter(adaptador);
+        //Realiza el cambio de evento según la selección
+        deportes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                if (eleccion1.isChecked()){
-                    adaptador = ArrayAdapter.createFromResource(this,R.array.deportesIndividuales, android.R.layout.simple_spinner_item );
-                } else if (eleccion2.isChecked()) {
-                    adaptador = ArrayAdapter.createFromResource(this,R.array.deportesEnPareja, android.R.layout.simple_spinner_item );
-                }
-                else {
-                    adaptador = ArrayAdapter.createFromResource(this,R.array.deportesGrupal, android.R.layout.simple_spinner_item );
-                }
-                adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                deportes.setAdapter(adaptador);
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                info.setText(deportes.getSelectedItem().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
             }
         });
 
 
+        //Modifica el adaptador según el boton seleccionado
+        elecciones.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                //Verificamos si la opción 1 está seleccionada
+                if (rb_eleccion1.isChecked()){
+                    //Usamos MainActivity.this para obtener el contexto correcto de la actividad
+                    adaptador = ArrayAdapter.createFromResource(MainActivity.this,
+                            R.array.deportesIndividuales, android.R.layout.simple_spinner_item);
+                    //Verificamos si la opción 2 está seleccionada
+                } else if (rb_eleccion2.isChecked()) {
+                    //Usamos MainActivity.this para obtener el contexto correcto de la actividad
+                    adaptador = ArrayAdapter.createFromResource(MainActivity.this,R.array.deportesEnPareja, android.R.layout.simple_spinner_item );
+                }
+                else {
+                    //Usamos MainActivity.this para obtener el contexto correcto de la actividad
+                    adaptador = ArrayAdapter.createFromResource(MainActivity.this,R.array.deportesGrupal, android.R.layout.simple_spinner_item );
+                }
+                adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                deportes.setAdapter(adaptador);
+
+                //Realiza el cambio de evento según la selección
+                deportes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        info.setText(deportes.getSelectedItem().toString());
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                });
+            }
+        });
 
     }
 }
